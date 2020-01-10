adc4ac744300dddee3fb4be40ce9e6f2f4cb57fb
Doctr (Travis CI) <drdoctr@users.noreply.github.com>
2020-01-10 17:56:24 +0000

    Update docs after building Travis build 57 of ke-zhang-rd/amostra
    The docs were built from the branch 'master' against the commit
    da0ef891defd9eeba79680ffd6e8d2725f8ce9d4.
    
    The Travis build that generated this commit is at
    https://travis-ci.org/ke-zhang-rd/amostra/jobs/635386278.
    
    The doctr command that was run is
    
        /home/travis/virtualenv/python3.6.7/bin/doctr deploy --deploy-repo ke-zhang-rd/ke-zhang-rd.github.io --deploy-branch-name master amostra

--- amostra/_static/documentation_options.js
+++ amostra/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '1.0.0.post65+g6dea7b7',
+    VERSION: '1.0.0.post64+gda0ef89',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
