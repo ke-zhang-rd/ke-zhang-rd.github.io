824d7e9857abe9a6ab55a7180c48aa3d18f82eb9
ke-zhang-rd <ke-zhang-rd@users.noreply.github.com>
2022-08-03 09:30:21 +0000

    deploy: ke-zhang-rd/nullspace@64ccbf95123cb5b8c666a82f02611cdbdad4f785

--- nullspace/_static/documentation_options.js
+++ nullspace/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.0.14.post0.dev0+g8f932ac',
+    VERSION: '0.0.15.post0.dev0+g64ccbf9',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
