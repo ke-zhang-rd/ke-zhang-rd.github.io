916e55504a310e956ac5a7723f79a3b54e579c46
ke-zhang-rd <ke-zhang-rd@users.noreply.github.com>
2022-08-16 07:21:49 +0000

    deploy: ke-zhang-rd/krystal@d1edb17d85d175dff8e7cb8cad24bc368ea6c4ac

--- krystal/_static/documentation_options.js
+++ krystal/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.post1+gc1ab7e0',
+    VERSION: '0.post1+gd1edb17',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
